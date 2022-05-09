759449174e4a67df9606a828488a919fe3493c3e
Julia Rosa <102997951+JuliaRosa014@users.noreply.github.com>
2022-05-09 17:43:56 -0300

    Add files via upload

---
+++ Projeto-10-Viajando-Num-Navio-main/Projeto-10-Viajando-Num-Navio-main/sketch.js
@@ -0,0 +1,38 @@
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-1.png",
+                            "ship-2.png","ship-1.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+
+  // Plano de fundo móvel
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
+}
+
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+
+  
+  //código para redefinir o plano de fundo
+  if(sea.x < 0){
+    sea.x = sea.width/8;
+  }
+    
+  drawSprites();
+}
